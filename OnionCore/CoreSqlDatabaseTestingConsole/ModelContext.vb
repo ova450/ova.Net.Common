0de91c27904b809f76de481e4a5186518aface87
ova450 <34611062+ova450@users.noreply.github.com>
2021-01-07 07:16:21 +0800

    added to previous fixing

---
+++ OnionCore/CoreSqlDatabaseTestingConsole/ModelContext.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Reflection
+Imports Model1
+Imports Model2
+Imports ova.Common.Core.DomainService.SqlService
+
+Public Class ModelContext1 : Inherits DbContextAbstract
+    Sub New()
+        MyBase.New(Assembly.GetAssembly(GetType(Class11)), New ConnectionString With {.dbname = "db1"})
+    End Sub
+End Class
+
+Public Class ModelContext2 : Inherits DbContextAbstract
+    Sub New()
+        MyBase.New(Assembly.GetAssembly(GetType(Class21)), New ConnectionString With {.dbname = "db2"})
+    End Sub
+End Class
+
+Public Class ModelContext : Inherits DbContextAbstract
+    Sub New()
+        MyBase.New(New ConnectionString With {.dbname = "db"})
+    End Sub
+End Class
+Public Class ModelContext0 : Inherits DbContextAbstract
+    Sub New()
+        MyBase.New("db0")
+    End Sub
+End Class
